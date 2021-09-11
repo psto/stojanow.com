15f760dc2f4ede76408f379d1e2d31baca557344
Piotr Stojanow <piotr.stojanow@gmail.com>
2021-09-11 23:46:38 +0200

    redesign post layout with tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,42 @@
 module.exports = {
+  important: "html", // Override typography plugin with more specific selectors.
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
         'brand-red': '#cc3535'
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            img: {
+              marginLeft: "auto",
+              marginRight: "auto"
+            },
+            color: '#1a1a1a',
+            a: {
+              color: '#cc3535',
+              '&:hover': {
+                color: '#ff7f00',
+              },
+            },
+            blockquote: {
+              fontStyle: 'italic',
+              fontWeight: 'normal',
+              color: '#333',
+              borderLeft: '3px solid #cc3535',
+              margin: '1em 1em',
+              padding: '1em 1em',
+            },
+            "blockquote p:first-of-type::before": {
+              content: "",
+            },
+            "blockquote p:first-of-type::after": {
+              content: "",
+            },
+          },
+        },
       }
     },
   },
@@ -13,5 +45,7 @@ module.exports = {
       brightness: ['hover', 'focus'],
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
 }
