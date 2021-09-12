42aaecf4b51484b2cb939edb7dead56b5fc67e83
Piotr Stojanow <piotr.stojanow@gmail.com>
2021-09-12 22:12:03 +0200

    remove unused scripts, styles and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,29 +5,29 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'brand-red': '#cc3535'
+        "brand-red": "#cc3535",
       },
       typography: {
         DEFAULT: {
           css: {
             img: {
               marginLeft: "auto",
-              marginRight: "auto"
+              marginRight: "auto",
             },
-            color: '#1a1a1a',
+            color: "#1a1a1a",
             a: {
-              color: '#cc3535',
-              '&:hover': {
-                color: '#ff7f00',
+              color: "#cc3535",
+              "&:hover": {
+                color: "#ff7f00",
               },
             },
             blockquote: {
-              fontStyle: 'italic',
-              fontWeight: 'normal',
-              color: '#333',
-              borderLeft: '3px solid #cc3535',
-              margin: '1em 1em',
-              padding: '1em 1em',
+              fontStyle: "italic",
+              fontWeight: "normal",
+              color: "#333",
+              borderLeft: "3px solid #cc3535",
+              margin: "1em 1em",
+              padding: "1em 1em",
             },
             "blockquote p:first-of-type::before": {
               content: "",
@@ -37,15 +37,13 @@ module.exports = {
             },
           },
         },
-      }
+      },
     },
   },
   variants: {
     extend: {
-      brightness: ['hover', 'focus'],
+      brightness: ["hover", "focus"],
     },
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
-}
+  plugins: [require("@tailwindcss/typography")],
+};
