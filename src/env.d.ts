280f7c8fde306708244fc6ad068b5aba182da5b5
Piotr Stojanow <piotr.stojanow@gmail.com>
2022-08-23 20:50:28 +0200

    feat: add types for env variables

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,15 @@
-/// <reference types="astro/client" />
+interface ImportMetaEnv {
+  readonly DB_PASSWORD: string;
+  readonly PUBLIC_POKEAPI: string;
+  readonly PUBLIC_GITHUB_USERNAME: string;
+  readonly PUBLIC_CONTACT_EMAIL: string;
+  readonly PUBLIC_LINKEDIN_PROFILE: string;
+  readonly PUBLIC_TWITTER_USERNAME: string;
+  readonly PUBLIC_DOMAIN: string;
+  readonly PUBLIC_SITE_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
