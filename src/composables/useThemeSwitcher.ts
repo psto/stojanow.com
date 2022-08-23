beab74aca0d5ee84c67901fe95fa625c48aa58cb
Piotr Stojanow <piotr.stojanow@gmail.com>
2022-08-23 21:02:39 +0200

    feat: add toggle theme composable

---
+++ src/composables/useThemeSwitcher.ts
@@ -0,0 +1,28 @@
+import { watchEffect } from 'vue';
+import { useStore } from '@nanostores/vue';
+import { themeStore, setTheme } from '~/stores/themeStore';
+
+const theme = useStore(themeStore);
+
+export function useThemeSwitcher() {
+  const toggleTheme = () => {
+    console.log(theme.value);
+    setTheme(theme.value === 'light' ? 'dark' : 'light');
+  };
+
+  watchEffect(() => {
+    if (typeof window !== 'undefined') {
+      if (theme.value === 'dark') {
+        window.document.documentElement.classList.add('dark');
+      } else {
+        window.document.documentElement.classList.remove('dark');
+      }
+      localStorage.setItem('theme', theme.value);
+    }
+  });
+
+  return {
+    theme,
+    toggleTheme,
+  };
+}
