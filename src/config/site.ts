a37dcab463276e74971810a9b64e727a5ca67c98
Piotr Stojanow <piotr.stojanow@gmail.com>
2022-08-23 20:34:34 +0200

    feat: add site config

---
+++ src/config/site.ts
@@ -0,0 +1,15 @@
+const site = {
+  author: 'Piotr Stojanow',
+  ogLanguage: 'en_GB',
+  siteLanguage: 'en-GB',
+  siteTitle: 'Piotr Stojanow',
+  siteUrl: import.meta.env ? import.meta.env.PUBLIC_SITE_URL : '',
+  icon: 'public/icon.png',
+  substackUrl:
+    'https://stojanow.substack.com/subscribe?simple=true&next=https%3A%2F%2Fstojanow.substack.com',
+  contactEmail: import.meta.env ? import.meta.env.PUBLIC_CONTACT_EMAIL : '',
+  githubUsername: import.meta.env ? import.meta.env.PUBLIC_GITHUB_USERNAME : '',
+  twitterUsername: import.meta.env ? import.meta.env.PUBLIC_TWITTER_USERNAME : '',
+};
+
+export default site;
