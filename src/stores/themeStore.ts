d7b4b08b4d7f8f1f9846d2f54d8127c88c887e72
Piotr Stojanow <piotr.stojanow@gmail.com>
2022-08-23 21:02:21 +0200

    feat: add theme nanostore

---
+++ src/stores/themeStore.ts
@@ -0,0 +1,7 @@
+import { atom, action } from 'nanostores';
+
+export const themeStore = atom(localStorage.getItem('theme') ?? 'dark');
+
+export const setTheme = action(themeStore, 'setTheme', (store, theme) => {
+  store.set(theme);
+});
