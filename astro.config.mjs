3a012e82ea8d6653d4978f50b51b01ca9676c9f4
Piotr Stojanow <piotr.stojanow@gmail.com>
2022-11-01 19:35:23 +0100

    feat: update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,27 @@
-import { defineConfig } from 'astro/config';
-import vue from '@astrojs/vue';
-import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import vue from '@astrojs/vue';
+import Icons from 'unplugin-icons/vite';
+import mdx from "@astrojs/mdx";
+import robotsTxt from 'astro-robots-txt';
+import compress from "astro-compress";
 
-import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), vue()]
+  site: 'https://stojanow.com',
+  experimental: {
+    integrations: true
+  },
+  integrations: [sitemap(), tailwind(), vue(), mdx(), compress(), robotsTxt({
+    sitemap: false
+  }), image()],
+  vite: {
+    plugins: [Icons()],
+    ssr: {
+      external: ['svgo']
+    }
+  }
 });
