1787a1bee577bf9dc5d3c87b39b53f42bd44010d
Piotr Stojanow <piotr.stojanow@gmail.com>
2023-01-31 21:08:49 +0100

    refactor: remove unused types & fix rss link

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,15 +1,12 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="@astrojs/image/client" />
 interface ImportMetaEnv {
-  readonly DB_PASSWORD: string;
-  readonly PUBLIC_POKEAPI: string;
   readonly PUBLIC_GITHUB_USERNAME: string;
   readonly PUBLIC_CONTACT_EMAIL: string;
   readonly PUBLIC_LINKEDIN_PROFILE: string;
   readonly PUBLIC_TWITTER_USERNAME: string;
   readonly PUBLIC_DOMAIN: string;
   readonly PUBLIC_SITE_URL: string;
-  readonly PUBLIC_TWITTER_BEARER_TOKEN: string
 }
 
 interface ImportMeta {
