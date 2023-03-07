86e85df44262c45a5071a9918f9a2157e202f06c
Piotr Stojanow <piotr.stojanow@gmail.com>
2023-03-07 21:34:04 +0100

    refactor: use DRY post schema for content

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,26 +1,22 @@
 import { z, defineCollection } from "astro:content";
 
+const postSchema = z.object({
+  title: z.string(),
+  date: z.string().or(z.date()),
+  description: z.string(),
+  draft: z.boolean().optional(),
+  image: z.string().optional(),
+  imageAlt: z.string().optional(),
+});
+
 const postCollection = defineCollection({
-  schema: z.object({
-    title: z.string(),
-    date: z.string().or(z.date()),
-    description: z.string(),
-    draft: z.boolean().optional(),
-    image: z.string().optional(),
-    imageAlt: z.string().optional(),
-  }),
+  schema: postSchema,
 });
 
 const noteCollection = defineCollection({
-  schema: z.object({
-    title: z.string(),
-    date: z.string().or(z.date()),
+  schema: postSchema.extend({
     dateRead: z.string().or(z.date()),
-    description: z.string(),
-    draft: z.boolean().optional(),
     amazon: z.string(),
-    image: z.string(),
-    imageAlt: z.string().optional(),
   }),
 });
 
